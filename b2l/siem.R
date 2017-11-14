3b00f7cea65e3203908c2d902838f5b63b93708f
rijpma <auke.rijpma@gmail.com>
2017-11-14 13:29:58 +0100

    fix colnames

--- b2l/siem.R
+++ b2l/siem.R
@@ -9,6 +9,8 @@ library("stringi")
 
 siem = readxl::read_excel("dat/urb pop 700 to 2000.xlsx", sheet = "Sheet2", skip = 2)
 data.table::setDT(siem)
+setnames(siem, c("UN country code", "transport location", "longtitude"), 
+    c("uncountrycode", "transportloc", "longitude"))
 
 comm = data.table::fread("dat/urb pop 700 to 2000 comments only.csv", skip = 2)
 lbls = data.table::fread("dat/urb pop 700 to 2000 getal_labels.csv")
@@ -24,12 +26,10 @@ siem = merge(siem, comm[, grep("city|\\d+y", names(comm)), with = F], by = 'city
 siem = merge(siem, lbls[, grep("city|\\d+y", names(lbls)), with = F], by = 'city', suffixes = c("", "_quality"))
 siem = siem[, -5]
 
-str(siem)
 siem_long = data.table::melt(siem, 
-    id.vars = c("city", "UN country code", "country", "latitude", "longtitude", "elevation", "transport location"),
+    id.vars = c("city", "uncountrycode", "country", "latitude", "longitude", "elevation", "transportloc"),
     variable.name = 'year', value.name = c("inhab", "comment", "quality"),
     measure.vars = data.table:::patterns("\\d+y$", "comment$", "quality$", cols = names(siem)))
-
 
 siem_long[, year := names(siem)[grep("\\d+y$", names(siem))][year]]
 siem_long[, year := as.integer(stringi::stri_extract_first_regex(year, "\\d+"))]
